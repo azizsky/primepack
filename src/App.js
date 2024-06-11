72cb1980e11b561dd63de1cd6f1d34efcb729d48
aszteam <asepbloodssa@gmail.com>
2024-06-11 20:34:10 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { IoMenuOutline, IoCloseOutline,
-IoLogoWhatsapp } from "react-icons/io5";
-import { MdOutlineLocalPolice } from "react-icons/md";
-import { GiPapers } from "react-icons/gi";
+import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
+import { CiBoxes } from "react-icons/ci";
+import { GiPapers, GiMagnifyingGlass} from "react-icons/gi";
+import { TbZoomMoney } from "react-icons/tb";
 import Menu from"./components/menu/menu.js";
 import Home from"./components/page/home/Home";
 import DusMakanan from"./components/page/dusmakanan/dusmakanan";
@@ -29,19 +30,19 @@ function App() {
             <Route path="/" element={<div className="home"><Home /></div>} />
             <Route path="/DusMakanan" element={<DusMakanan />}/>
          </Routes>
-      </Router>
       
       <footer>
          <div className="foter">
+            <a href="#produk"><CiBoxes /><h5>Produk</h5></a>
             <a href="#bahan"><GiPapers /><h5>Bahan</h5></a>
-            <a href="#laminet"><GiPapers /><h5>Laminasi</h5></a>
-            <a href="#foil"><GiPapers /><h5>Foil</h5></a>
-            <a href="#cut"><GiPapers /><h5>Cutting</h5></a>
-            <a href="#emboss"><MdOutlineLocalPolice /><h5>Emboss</h5></a>
-            <a href="#deboss"><MdOutlineLocalPolice /><h5>Deboss</h5></a>
-            <a href="#calk"><MdOutlineLocalPolice /><h5>Harga</h5></a>
+            <a href="#laminasi"><GiMagnifyingGlass /><h5>info produk</h5></a>
+            <a href="#calk"><TbZoomMoney /><h5>Harga</h5></a>
+            <Link to="/orders">
+            <MdOutlineShoppingCartCheckout /><h5>order</h5>
+            </Link>
          </div>
       </footer>
+      </Router>
       
     </div>
   );
